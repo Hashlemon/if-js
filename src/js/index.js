76b92cf83895e8b630b5263fdd629f9091c595a8
Hashlemon <arseni.yahorchanka@gmail.com>
2021-04-16 17:15:30 +0300

    Done Polindrome task

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
-let user = 'John Doe'
+function palindrome(word) {
+    let arr = word.split('');
+    let revArr = word.split('').reverse();
 
-console.log(user)
+    for (i =0; i < arr.length; i++) {
+        return arr[i] === revArr[i];
+    }
+}
+
+let word_1 = "шалаш";
+let word_2 = "anna";
+let word_3 = "шашлык";
+
+
+let isPolindrome = palindrome(word_1)
+console.log(isPolindrome) //true
+
+isPolindrome = palindrome(word_2)
+console.log(isPolindrome) //true
+
+isPolindrome = palindrome(word_3)
+console.log(isPolindrome) //false
