1271fbbfc4f5a3e7128499b028e0e99519ef3b1f
Hashlemon <arseni.yahorchanka@gmail.com>
2021-04-16 17:39:28 +0300

    Done Palindrome task bugfix

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,27 @@
 function palindrome(word) {
     let arr = word.split('');
     let revArr = word.split('').reverse();
-
+    let isEqual = false
     for (i =0; i < arr.length; i++) {
-        return arr[i] === revArr[i];
-    }
+        isEqual = arr[i] === revArr[i];
+        console.log('from func ' + isEqual)
+            if (isEqual === false)
+                break;
+        }
+    return isEqual === true;
+
+
 }
 
-let word_1 = "шалаш";
-let word_2 = "anna";
-let word_3 = "шашлык";
+
+let word_1 = "ша2ш";
+let word_2 = "an2na";
+let word_3 = "anna";
 
 
 let isPolindrome = palindrome(word_1)
-console.log(isPolindrome) //true
-
+console.log(isPolindrome)
 isPolindrome = palindrome(word_2)
-console.log(isPolindrome) //true
-
+console.log(isPolindrome)
 isPolindrome = palindrome(word_3)
-console.log(isPolindrome) //false
+console.log(isPolindrome)
